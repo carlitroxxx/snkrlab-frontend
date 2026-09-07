ce16a7d02727f73ff21e22f356fbfe3632b71a43
carlitroxxx <ca.moil@duocuc.cl>
2026-09-06 22:10:04 -0300

    Version 1.2.0: MsalProvider y BrowserRouter en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { PublicClientApplication, EventType } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./auth/AuthConfig";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const msalInstance = new PublicClientApplication(msalConfig);
+
+await msalInstance.initialize();
+const accounts = msalInstance.getAllAccounts();
+if (accounts.length > 0) {
+    msalInstance.setActiveAccount(accounts[0]);
+}
+
+msalInstance.addEventCallback((event) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
+        msalInstance.setActiveAccount(event.payload.account);
+    }
+});
+
+await msalInstance.handleRedirectPromise().catch((error) => {
+    console.error("Error procesando el redirect de MSAL:", error);
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <MsalProvider instance={msalInstance}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </MsalProvider>
+    </React.StrictMode>
+);
